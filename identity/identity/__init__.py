42df5a7218caa8eac7bdf92125b013ddc74c1040
dpausp <dpausp@posteo.de>
2016-11-21 17:42:43 +0000

    run configurations.setup() in identity __init__ if CELERY_WORKER env var is set
    The unconditional configurations.setup() has been removed in 638acf231ef4e8489bd66b9befc0d0443a0deba9,
    but we need it if the code is run from as celery worker process.

--- identity/identity/__init__.py
+++ identity/identity/__init__.py
@@ -1,10 +1,17 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 from django.conf import settings
 import os, configurations
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'identity.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Development')
+
+is_celery_worker = os.getenv("CELERY_WORKER")
+
+if is_celery_worker:
+    print("running in celery worker")
+    import configurations
+    configurations.setup()
 
 if settings.USE_CELERY:
     # This will make sure the app is always imported when
