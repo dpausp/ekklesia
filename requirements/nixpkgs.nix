b147df0accdc1c379f40a7d2caaf1d148306b4d5
dpausp <dpausp@posteo.de>
2017-07-04 15:27:46 +0000

    update nixpkgs

--- requirements/nixpkgs.nix
+++ requirements/nixpkgs.nix
@@ -1,4 +1,4 @@
 let
-	pkgUrl = "https://d3g5gsiof5omrk.cloudfront.net/nixpkgs/nixpkgs-17.09pre106045.7369fd0b51/nixexprs.tar.xz";
+	pkgUrl = "https://d3g5gsiof5omrk.cloudfront.net/nixpkgs/nixpkgs-17.09pre110215.7d36ae33df/nixexprs.tar.xz";
 in
 	import ( builtins.fetchTarball pkgUrl ) {}
